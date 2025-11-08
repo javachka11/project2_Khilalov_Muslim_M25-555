a8c551c66f7609d2c36518e6bf45bd3567f4d18c
Muslim Khilalov <hilalov@yandex.ru>
2025-11-08 19:35:05 +0300

    Adding function welcome()

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,5 @@
+import prompt
+
+def welcome():
+    command = prompt.string('Введите команду: ')
+    return command
--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+from src.primitive_db.engine import welcome
+
 def main():
     print('DB project is running!')
+    command = welcome()
+    print(command)
 
 if __name__ == '__main__':
     main()
